e0969a20e5021c8055d9e841af3b9da39fea6ed3
Rafaelaltieris <raphaelaltieris@gmail.com>
2025-10-06 14:34:17 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciadorDeTarefasGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GerenciadorDeTarefasGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
